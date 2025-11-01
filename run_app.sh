1af8df4fd4e43668cd7f15c5fad820f69c04cf39
NikhielR108 <nramraj108@gmail.com>
2025-11-01 15:42:45 +0200

    added code to kill the backend once the frontend is stopped

--- run_app.sh
+++ run_app.sh
@@ -10,10 +10,14 @@ trap "kill 0" EXIT
 
 cd src/backend
 bash ./run_backend.sh &
-sleep 3 
+BACKEND_PID=$!
 
 
 # start the frontend 
 
 cd ../frontend
 bash ./run_frontend.sh
+
+
+# When frontend exits, kill backend automatically
+kill $BACKEND_PID
